ebf6e7cb97a8bf3bec515e4001ade858c7e7acf7
soloserg <solosergit@gmail.com>
2022-07-23 11:04:12 +0300

    update 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const  addElementsList  =  ingredients. map (element  =>  {
+  const  newIngredient  =  document.createElement('li');
+  newIngredient. classList.add('item');
+  newIngredient.textContent = element;
+  return  newIngredient;
+} ) ;
+
+const listIngredients  =  document. querySelector ('ul#ingredients') ;
+listIngredients.append(... addElementsList) ;
+
+
+console.log(listIngredients);
+
+
+
